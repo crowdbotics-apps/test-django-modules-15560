3fb8789bf1fb099251c2104aa3b7a3478d40b611
Andrei Elizaga <naaeliz@gmail.com>
2020-04-10 04:52:15 +0800

    Add User admin.

--- users/admin.py
+++ users/admin.py
@@ -12,6 +12,11 @@ class UserAdmin(auth_admin.UserAdmin):
 
     form = UserChangeForm
     add_form = UserCreationForm
-    fieldsets = (("User", {"fields": ("name",)}),) + auth_admin.UserAdmin.fieldsets
-    list_display = ["username", "name", "is_superuser"]
+    fieldsets = (
+        ("User", {
+            "fields": ("name", 'user_type', 'address', 'phone_number',
+                        'business_name')
+        }),
+    ) + auth_admin.UserAdmin.fieldsets
+    list_display = ["username", "name", "is_superuser", 'user_type']
     search_fields = ["name"]
